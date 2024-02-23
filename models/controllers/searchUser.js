8912b8f8b7c4d646b7aa940a94b03c19be4ffe39
girikanchan <akira.giri2001@gmail.com>
2024-02-23 15:17:26 +0530

    search profile

--- models/controllers/searchUser.js
+++ models/controllers/searchUser.js
@@ -1,8 +1,44 @@
-const bcrypt = require('bcrypt');
 const connection = require('../db');
 const jwt = require('jsonwebtoken');
+
 module.exports = async (req, res) => {
+    try {
+        // Retrieve token from cookies
+        const token = req.cookies.accessToken;
 
+        if (!token) {
+            return res.status(401).json("Not logged in");
+        }
 
+        // Verify the JWT token
+        jwt.verify(token, "secretkey", async function (err, user) {
+            if (err) {
+                return res.status(300).json("Invalid Token");
+            }
+
+            const { username } = req.body;
+
+            // Check if email and password are provided
+            if (!username) {
+                return res.status(400).json({ error: 'User name required to search user name.' });
+            }
     
+            // Query database to find user by email
+            const getUserQuery = 'SELECT * FROM task2db WHERE email = ?';
+            connection.query(getUserQuery, [username], async (err, results) => {
+                if (err) {
+                    console.error('Error retrieving user:', err);
+                    return res.status(500).json({ error: 'Error retrieving user' });
+                }
+    
+                if (results.length === 0) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                return res.status(200).json("Profile found");
+            });
+        });
+    } catch (error) {
+        console.error('Error retrieving posts:', error);
+        res.status(500).json({ error: 'Error retrieving posts' });
+    }
 };
--- models/controllers/seepostComments.js
+++ models/controllers/seepostComments.js
@@ -1,7 +1,35 @@
-
-const bcrypt = require('bcrypt');
 const connection = require('../db');
 const jwt = require('jsonwebtoken');
-module.exports = async (req, res) => {
 
+module.exports = async (req, res) => {
+    try {
+        // Retrieve token from cookies
+        const token = req.cookies.accessToken;
+
+        if (!token) {
+            return res.status(401).json("Not logged in");
+        }
+
+        // Verify the JWT token
+        jwt.verify(token, "secretkey", async function (err, user) {
+            if (err) {
+                return res.status(300).json("Invalid Token");
+            }
+
+           //const { postId } = req.body; //sending this postId from /post (current api) to /comments api onclicking comment button.
+
+            // Query posts from database for the authenticated user
+            const postquery = 'SELECT p.*, u.email as user_id  FROM post AS p  JOIN task2db AS u ON (u.id = p.id)  WHERE u.id = ? ORDER BY p.updated_time DESC';
+
+            connection.query(postquery, [user.id], (err, posts) => {
+                if (err) {
+                    return res.status(500).json(err);
+                }
+                return res.status(200).json(posts);
+            });
+        });
+    } catch (error) {
+        console.error('Error retrieving posts:', error);
+        res.status(500).json({ error: 'Error retrieving posts' });
+    }
 };
